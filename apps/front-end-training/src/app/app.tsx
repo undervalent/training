07e0e4d5875aa7ccb193d53f4667537df35ddb84
Jon Ochoa <jon_ochoa@intuit.com>
2024-05-08 08:37:16 -0700

    added basic setup

---
+++ apps/front-end-training/src/app/app.tsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+
+export function App() {
+  return (
+    <>
+    <header>
+        <h1>Submission form</h1>
+      </header>
+      <section>
+        This is the form
+      </section>
+      </>
+  );
+}
+
+export default App;
